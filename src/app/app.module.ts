df7547714764d94e8a9f8599594dcc1d5ad3ae5e
Hristiyan Mitov <hriskohm@gmail.com>
2022-04-28 21:58:15 +0300

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
