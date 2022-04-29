f3d82c44f0d091061bebb87b27d927bbf5b6ddd3
hmitov <hmitov@hedgeserv.com>
2022-04-29 12:40:52 +0300

    new files

---
+++ src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cockpit',
+  templateUrl: './cockpit.component.html',
+  styleUrls: [ './cockpit.component.css' ]
+})
+export class CockpitComponent implements OnInit {
+  serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerName = '';
+  newServerContent = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddServer() {
+    //this.
+  }
+
+  onAddBlueprint() {
+
+  }
+
+}
